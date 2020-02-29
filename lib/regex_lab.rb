3aafb491419bc1b90afe1f68faa0874dcc558e9f
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-29 17:15:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,11 +11,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\w.{5}/)
+  text.scan{|word| word = (/\w{5}/)}
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\w+[A-Za-z]+./)
+  text.match(/\w+[A-Z]+./)
     false 
 end
 
