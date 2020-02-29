c914f1257cc3e74bfbad0ed19a69414d358975c8
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-29 17:07:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,6 @@
 def starts_with_a_vowel?(word)
-  if word == word.match(/\w[aeiou]/)
-    true
-  else word == word.match(/\w[^aeiou]/)
+  word.match(/\w[^aeiou]/)
     false 
-  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
